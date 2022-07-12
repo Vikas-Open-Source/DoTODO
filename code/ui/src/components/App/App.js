06bdab9d07e0807345c36b99a58c3f6e89a9c4b7
Vikas Kumar <krvikas1011@gmail.com>
2022-07-12 15:38:16 +0530

    Added login page

--- code/ui/src/components/App/App.js
+++ code/ui/src/components/App/App.js
@@ -1,15 +1,23 @@
 import './App.css';
 import '../Dashboard/Dashboard'
-import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Dashboard from '../Dashboard/Dashboard';
+import React, { useState } from 'react';
+import Login from '../Login/Login'
 
 function App() {
+  const [token, setToken] = useState()
+
+  if(!token){
+    return <Login setToken={setToken}/>
+  }
+
   return (
     <div className='wrapper'>
       <h1>DoTODO Application</h1>
       <BrowserRouter>
         <Routes>
-          <Route exact path="/dashboard" element={<Dashboard />}/>
+          <Route exact path="/dashboard" element={<Dashboard />} />
         </Routes>
       </BrowserRouter>
     </div>
