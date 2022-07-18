2947d4b977880ee1f1a89dd08a856fbb9410f5fc
Vikas Kumar <krvikas1011@gmail.com>
2022-07-18 20:50:04 +0530

    added login api call logic

--- code/ui/src/components/Login/Login.js
+++ code/ui/src/components/Login/Login.js
@@ -2,14 +2,35 @@ import React, { useState } from 'react'
 import PropTypes from 'prop-types';
 import './Login.css'
 
+
+async function loginUser(credentials){
+    var loginAPIURL = 'http://localhost:8080/login'
+    return fetch(loginAPIURL, {
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body: credentials
+    }).then(data => data.json())
+}
+
 export default function Login({ setToken }) {
     const [username, setUsername] = useState();
     const [password, setPassword] = useState();
 
+    const handleSubmit = async event => {
+        event.preventDefault();
+        const token = await loginUser({
+            username,
+            password
+        });
+        setToken(token);
+    }
+
     return (
         <div className='login-wrapper'>
             <h1>Please Login</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>
                     <p>Username</p>
                     <input type="text" onChange={event => setUsername(event.target.value)} />
