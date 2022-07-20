5817c92f3770460062cee050bc9dfa438f99cc6f
Vikas Kumar <krvikas1011@gmail.com>
2022-07-20 13:22:19 +0530

    Added paper component

--- code/ui/src/components/Login/Login.js
+++ code/ui/src/components/Login/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types';
+import Paper from '@mui/material/Paper';
 import './Login.css'
 
 
@@ -33,22 +34,24 @@ export default function Login({ setToken }) {
     }
 
     return (
-        <div className='login-wrapper'>
-            <h1>Please Login</h1>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    <p>Username</p>
-                    <input type="text" onChange={event => setUsername(event.target.value)} />
-                </label>
-                <label>
-                    <p>Password</p>
-                    <input type="password" onChange={event => setPassword(event.target.value)} />
-                </label>
-                <div>
-                    <button type='submit'>Submit</button>
-                </div>
-            </form>
-        </div>
+        <Paper elevation={3}>
+            <div className='login-wrapper'>
+                <h1>Please Login</h1>
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        <p>Username</p>
+                        <input type="text" onChange={event => setUsername(event.target.value)} />
+                    </label>
+                    <label>
+                        <p>Password</p>
+                        <input type="password" onChange={event => setPassword(event.target.value)} />
+                    </label>
+                    <div>
+                        <button type='submit'>Submit</button>
+                    </div>
+                </form>
+            </div>
+        </Paper>
     )
 }
 
